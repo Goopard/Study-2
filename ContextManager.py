0ab69b7204471ca8198cc541f33c5f58b011dbcd
Goopard <nick.amirkhanov@gmail.com>
2018-04-17 20:39:58 +0300

    HW9#2

--- ContextManager.py
+++ ContextManager.py
@@ -6,14 +6,13 @@ import traceback
 
 class ExecutionInfo:
     """This is a context manager that prints the information (starting time of the execution, time elapsed on the
-    execution, information on an exception raised in the code (if such occured)) on some code wrapped in this manager in
-    some given file. For example:
+        execution, information on an exception raised in the code (if such occured)) on some code wrapped in this manager in
+        some given file. For example:
+        >>>with ExecutionInfo('somefile.txt'):
+        >>>     pass
 
-    >>>with ExecutionInfo('somefile.txt'):
-    >>>     pass
-
-    Will print all the information in the file somefile.txt.
-    """
+        Will print all the information in the file somefile.txt.
+        """
     def __init__(self, path):
         """Constructor of class ExecutionInfo. Takes one parameter: path to the file.
 
@@ -29,8 +28,9 @@ class ExecutionInfo:
         self.execution_datetime = datetime.datetime.now()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        """Method called on the exiting of the code, wrapped by this context manager. Here we calculate time elapsed on
-        the execution, open the output file, print all the required information and close the file.
+        """Method called on the exiting of the code wrapped by this context manager. Calculates time elapsed on
+        the execution and if some exception was raised during the execution opens the output file, prints all the
+        required information and closes the file.
 
         :param exc_type: Type of the exception raised in the wrapped code.
         :type exc_type: Exception.
@@ -41,17 +41,16 @@ class ExecutionInfo:
         :return None.
         """
         self.time_elapsed = time.time() - self.time_start
-        self.file = open(self.file_path, 'w')
-        print('Information on the code executed on {}:\nTime elapsed on the execution of the code: {}'
-              .format(self.execution_datetime, self.time_elapsed),
-              file=self.file)
-        if exc_type is None:
-            print('No exceptions raised during the execution of the code.', file=self.file)
-        else:
-            print('Exception raised!\nTraceback (most recent call last):', file=self.file)
+        if exc_type is not None:
+            self.file = open(self.file_path, 'w')
+            print('Information on the code executed on {}:\n'
+                  'Time elapsed on the execution of the code: {}\n'
+                  'Exception raised:\n'
+                  'Traceback (most recent call last):'
+                  .format(self.execution_datetime, self.time_elapsed), file=self.file)
             traceback.print_tb(exc_tb, file=self.file)
             print('{}: {}'.format(exc_type.__name__, exc_val), file=self.file)
-        self.file.close()
+            self.file.close()
 
 
 with ExecutionInfo('feedback.txt'):
